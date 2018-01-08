0d7b53a4a50e8610fcc7a018d200125c5ae553bd
Chris Gullickson <cgullickson5@gmail.com>
2018-01-08 23:04:18 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,7 @@
+class CreateShows < ActiveRecord::migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
